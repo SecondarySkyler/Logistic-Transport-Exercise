4d94e1a1c7756c151b770523abd6f625efb44b5e
Cristian Murtas <skylerhunter6@gmail.com>
2020-11-26 18:20:38 +0100

    Made small changes

--- src/Customer/Cliente.java
+++ src/Customer/Cliente.java
@@ -1,8 +1,8 @@
 package Customer;
 
-import Gestore.Città;
+import Gestore.City;
 import Gestore.Merce;
-import Gestore.Viaggio;
+import Gestore.Shipment;
 
 public class Cliente {
     /* This class represent the client class, it should be able
@@ -14,20 +14,20 @@ public class Cliente {
         this.name = name;
     }
 
-    public Viaggio requireShipment(Città from, Città destination, int quantity,Merce.Tipo t) {
+    public Shipment requireShipment(City from, City destination, int quantity, Merce.Tipo t) {
         return createShipment(from, destination, quantity, t);
     }
 
 
     /**
      * Method that allows a customer to place a shipment
-     * @param from the Città where to pickup the shipment
-     * @param destination the Città where to end the shipment
+     * @param from the City where to pickup the shipment
+     * @param destination the City where to end the shipment
      * @param quantity of the Merce to ship
      * @param t Tipo of Merce
      */
-    private Viaggio createShipment(Città from, Città destination, int quantity,Merce.Tipo t) {
-        Viaggio requiredViaggio = new Viaggio(from, destination);
+    private Shipment createShipment(City from, City destination, int quantity, Merce.Tipo t) {
+        Shipment requiredViaggio = new Shipment(from, destination);
         Merce merceToTransport = new Merce(quantity, t);
         requiredViaggio.addMerce(merceToTransport);
         return requiredViaggio;
--- src/Gestore/Percorso.java
+++ src/Gestore/Percorso.java
@@ -2,17 +2,23 @@ package Gestore;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-import java.util.NoSuchElementException;
 
 public class Percorso {
-    ArrayList<Viaggio> percorsi;
+
+    /**
+     * lo scopo di questa classe e quello di contenere tutte le spedizioni istanziate
+     * composte da istanze di altri tipi (City, Autocarro, Merce, Calendar)
+     */
+
+
+
+    ArrayList<Shipment> percorsi;
 
     public Percorso() {
-        this.percorsi = new ArrayList<Viaggio>();
+        this.percorsi = new ArrayList<Shipment>();
     }
 
-    public void addViaggio (Viaggio v) {
+    public void addViaggio (Shipment v) {
         this.percorsi.add(v);
     }
 
@@ -33,44 +39,44 @@ public class Percorso {
         }
     }
 
-    /**
-     * Method to search a Prenotazione, it returns the trip with the Città c associated
-     * @param c the Città used to retrieve inforamtion about
-     */
-    public void cercaPrenotazione(Città c) {
-        int length = this.percorsi.size();
-        ArrayList result = new ArrayList();
-        for (int i = 0; i < length; i++) {
-            if (c == percorsi.get(i).retrieveName()) {
-                result.add(percorsi.get(i).retrieveInfo());
-            }
-        }
-        System.out.println(Arrays.toString(result.toArray()));
-
-    }
-
-    public void cercaPrenotazione(String t) {
-        int length = this.percorsi.size();
-        ArrayList result = new ArrayList();
-        for (int i = 0; i < length; i++) {
-            if (t == percorsi.get(i).retrieveTarga()) {
-                result.add(percorsi.get(i).retrieveInfo());
-            }
-        }
-        System.out.println(Arrays.toString(result.toArray()));
-
-    }
-
-    public void cercaPrenotazione(Merce.Tipo m) {
-        int length = this.percorsi.size();
-        ArrayList result = new ArrayList();
-        for (int i = 0; i < length; i++) {
-            if (m == percorsi.get(i).retrieveMerce()) {
-                result.add(percorsi.get(i).retrieveInfo());
-            }
-        }
-        System.out.println(Arrays.toString(result.toArray()));
-    }
+//    /**
+//     * Method to search a Prenotazione, it returns the trip with the Città c associated
+//     * @param c the Città used to retrieve inforamtion about
+//     */
+//    public void cercaPrenotazione(City c) {
+//        int length = this.percorsi.size();
+//        ArrayList result = new ArrayList();
+//        for (int i = 0; i < length; i++) {
+//            if (c == percorsi.get(i).retrieveName()) {
+//                result.add(percorsi.get(i).retrieveInfo());
+//            }
+//        }
+//        System.out.println(Arrays.toString(result.toArray()));
+//
+//    }
+//
+//    public void cercaPrenotazione(String t) {
+//        int length = this.percorsi.size();
+//        ArrayList result = new ArrayList();
+//        for (int i = 0; i < length; i++) {
+//            if (t == percorsi.get(i).retrieveTarga()) {
+//                result.add(percorsi.get(i).retrieveInfo());
+//            }
+//        }
+//        System.out.println(Arrays.toString(result.toArray()));
+//
+//    }
+//
+//    public void cercaPrenotazione(Merce.Tipo m) {
+//        int length = this.percorsi.size();
+//        ArrayList result = new ArrayList();
+//        for (int i = 0; i < length; i++) {
+//            if (m == percorsi.get(i).retrieveMerce()) {
+//                result.add(percorsi.get(i).retrieveInfo());
+//            }
+//        }
+//        System.out.println(Arrays.toString(result.toArray()));
+//    }
 
 
 }
