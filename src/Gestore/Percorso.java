3afd1b2a532b04a1c537370507e0ca1e55f9a189
Cristian Murtas <skylerhunter6@gmail.com>
2020-11-27 12:03:49 +0100

    Made some changes to Percorso, changed structure

--- src/Gestore/Percorso.java
+++ src/Gestore/Percorso.java
@@ -1,76 +1,117 @@
 package Gestore;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.NoSuchElementException;
+import Gestore.Eccezioni.CityNotFoundException;
+import java.util.*;
 
 public class Percorso {
-    ArrayList<Viaggio> percorsi;
-
-    public Percorso() {
-        this.percorsi = new ArrayList<Viaggio>();
-    }
-
-    public void addViaggio (Viaggio v) {
-        this.percorsi.add(v);
-    }
-
-    public int getSize() {
-        return this.percorsi.size();
-    }
-
+    ArrayList<Città> percorsi;
+    private Città partenza;
+    private Città destinazione;
 
     /**
-     * Method to see every single Viaggio
+     * Costruttore per istanziare un Percorso
+     * @param p la citta di PARTENZA
+     * @param d la citta di ARRIVO
+     * E' importante l'ordine delle citta p e d devono rispettare le regole dei @param
      */
-    public void showList(){
-        int length = this.percorsi.size();
-        ArrayList info = new ArrayList();
-        for (int i = 0; i < length; i++) {
-            info = percorsi.get(i).retrieveInfo();
-            System.out.println(Arrays.toString(info.toArray()));
-        }
+    public Percorso(Città p, Città d) {
+        partenza = p;
+        destinazione = d;
+        this.percorsi = new ArrayList();
+        this.percorsi.add(this.partenza);
+        this.percorsi.add(this.destinazione);
     }
 
     /**
-     * Method to search a Prenotazione, it returns the trip with the Città c associated
-     * @param c the Città used to retrieve inforamtion about
+     * Aggiunge una nuova tappa al percorso
+     * @param c la citta da aggiungere
+     *  N.B. la citta viene aggiunta tra la Partenza e la Destinazione, perciò è necessario
+     *  inserirle in ordine di distanza rispetto alla partenza
      */
-    public void cercaPrenotazione(Città c) {
-        int length = this.percorsi.size();
-        ArrayList result = new ArrayList();
-        for (int i = 0; i < length; i++) {
-            if (c == percorsi.get(i).retrieveName()) {
-                result.add(percorsi.get(i).retrieveInfo());
-            }
-        }
-        System.out.println(Arrays.toString(result.toArray()));
-
+    public void addTappa (Città c) {
+        this.percorsi.add(percorsi.size() - 1, c);
     }
 
-    public void cercaPrenotazione(String t) {
-        int length = this.percorsi.size();
-        ArrayList result = new ArrayList();
-        for (int i = 0; i < length; i++) {
-            if (t == percorsi.get(i).retrieveTarga()) {
-                result.add(percorsi.get(i).retrieveInfo());
+    public void changeTappa(Città c, Città r) throws CityNotFoundException {
+        if (percorsi.contains(r)) {
+            for (int i = 0; i < percorsi.size(); i++) {
+                if (percorsi.get(i) == r) {
+                    percorsi.remove(r);
+                    percorsi.add(i, c);
+                }
             }
-        }
-        System.out.println(Arrays.toString(result.toArray()));
-
+        } else
+            throw new CityNotFoundException();
     }
 
-    public void cercaPrenotazione(Merce.Tipo m) {
-        int length = this.percorsi.size();
-        ArrayList result = new ArrayList();
-        for (int i = 0; i < length; i++) {
-            if (m == percorsi.get(i).retrieveMerce()) {
-                result.add(percorsi.get(i).retrieveInfo());
-            }
+    public void showPercorsi() {
+        for (int i = 0; i < percorsi.size(); i++) {
+            String nomeCitta = percorsi.get(i).nome;
+            System.out.print(nomeCitta + ", ");
         }
-        System.out.println(Arrays.toString(result.toArray()));
     }
+
+    public void search(Città c) {
+        Iterator it = percorsi.iterator();
+        while (it.hasNext()) {
+            Object x = it.next();
+            if (x instanceof Città)
+                System.out.println(c.nome);
+        }
+    }
+
+
+//
+//    /**
+//     * Method to see every single Viaggio
+//     */
+//    public void showList(){
+//        int length = this.percorsi.size();
+//        ArrayList info = new ArrayList();
+//        for (int i = 0; i < length; i++) {
+//            info = percorsi.get(i).retrieveInfo();
+//            System.out.println(Arrays.toString(info.toArray()));
+//        }
+//    }
+//
+//    /**
+//     * Method to search a Prenotazione, it returns the trip with the Città c associated
+//     * @param c the Città used to retrieve inforamtion about
+//     */
+//    public void cercaPrenotazione(Città c) {
+//        int length = this.percorsi.size();
+//        ArrayList result = new ArrayList();
+//        for (int i = 0; i < length; i++) {
+//            if (c == percorsi.get(i).retrieveName()) {
+//                result.add(percorsi.get(i).retrieveInfo());
+//            }
+//        }
+//        System.out.println(Arrays.toString(result.toArray()));
+//
+//    }
+//
+//    public void cercaPrenotazione(String t) {
+//        int length = this.percorsi.size();
+//        ArrayList result = new ArrayList();
+//        for (int i = 0; i < length; i++) {
+//            if (t == percorsi.get(i).retrieveTarga()) {
+//                result.add(percorsi.get(i).retrieveInfo());
+//            }
+//        }
+//        System.out.println(Arrays.toString(result.toArray()));
+//
+//    }
+//
+//    public void cercaPrenotazione(Merce.Tipo m) {
+//        int length = this.percorsi.size();
+//        ArrayList result = new ArrayList();
+//        for (int i = 0; i < length; i++) {
+//            if (m == percorsi.get(i).retrieveMerce()) {
+//                result.add(percorsi.get(i).retrieveInfo());
+//            }
+//        }
+//        System.out.println(Arrays.toString(result.toArray()));
+//    }
 
 
 }
--- src/Manager/main.java
+++ src/Manager/main.java
@@ -1,6 +1,7 @@
 package Manager;
 import Gestore.*;
-import Customer.Cliente;
+import Gestore.Eccezioni.CityNotFoundException;
+
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -10,65 +11,42 @@ public class main {
 
     //TODO: Organize main with more method to create instances
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws CityNotFoundException {
 
         Città Udine = new Città("Udine");
         Città Milano = new Città("Milano");
+        Città Mestre = new Città("Mestre");
+        Città Bergamo = new Città("Bergamo");
+        Città Manzano = new Città("Manzano");
+        Città Torino = new Città("Torino");
+        Percorso percorso1 = new Percorso(Udine, Milano);
         int quantità = 300;
         Merce.Tipo t = Merce.Tipo.Acciaio;
+        Merce merce = new Merce(quantità, t);
 
         String targa = "123";
         int  capacity = 1000;
         Merce.Tipo type = Merce.Tipo.Acciaio;
         Autocarro truck1 = new Autocarro(targa, capacity, type);
 
-        Cliente client = new Cliente("Cristian");
-        Viaggio required = client.requireShipment(Udine, Milano, quantità, t);
-        required.addVehicle(truck1);
+//        Cliente client = new Cliente("Cristian");
+//        Viaggio required = client.requireShipment(Udine, Milano, quantità, t);
+//        required.addVehicle(truck1);
+//        required.addMerce(merce);
+//        required.info();
+        percorso1.addTappa(Mestre);
+        percorso1.addTappa(Bergamo);
+        percorso1.changeTappa(Manzano, Bergamo);
+        percorso1.showPercorsi();
 
-
-        Percorso requiredShipment = new Percorso();
-        requiredShipment.addViaggio(required);
-        requiredShipment.showList();
-        //requiredShipment.cercaPrenotazione(Udine);
-
-
-
-        /*
-        Viaggio firstSpedition = new Viaggio(Udine, Milano); //creo viaggio
-        Città Roma = new Città("Roma");
-        Città Napoli = new Città("Napoli");
-        Viaggio rm_np = new Viaggio(Roma, Napoli);
-        int quantity = 100;
-        Percorso percorsi = new Percorso();
-        Merce merceDaTrasporto = new Merce(quantity, Merce.Tipo.Acciaio); // creo merce
-        Flotta equip = new Flotta();
-        String targa = "ABCDEF";
-        int truckCapacity = 1000;
-        Autocarro camion = new Autocarro(targa, truckCapacity, merceDaTrasporto);
-        equip.addTruck(camion);
-        String id = "cioa";
-        int kg = 10;
-        int q = 10;
-        Merce merce = new Merce(q, Merce.Tipo.Gas);
-        Autocarro iveco = new Autocarro(id, kg ,merce);
-        equip.addTruck(iveco);
-        rm_np.addVehicle(iveco);
-        rm_np.addMerce(merce);
-        firstSpedition.addVehicle(camion);
-        firstSpedition.addMerce(merceDaTrasporto);
-        //firstSpedition.retrieveInfo();
-        //System.out.println(Arrays.toString(firstSpedition.retrieveInfo().toArray()));
-        percorsi.addViaggio(firstSpedition);
-        percorsi.addViaggio(rm_np);
-        percorsi.showList();
-        percorsi.cercaPrenotazione("ABCDEF");
-        percorsi.cercaPrenotazione(Merce.Tipo.Gas);*/
+//        Percorso requiredShipment = new Percorso();
+//        requiredShipment.addViaggio(required);
+//        requiredShipment.showList();
+//        //requiredShipment.cercaPrenotazione(Udine);
 
 
 
-        /*System.out.println(equip.getSize());
-        System.out.println(percorsi.getSize());*/
+
 
 
     }
