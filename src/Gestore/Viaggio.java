f786239b9582dd5ad92c037ef9544fc29afa5cd0
Cristian Murtas <skylerhunter6@gmail.com>
2020-11-27 12:05:37 +0100

    Now storing all the information in a collection

--- src/Gestore/Viaggio.java
+++ src/Gestore/Viaggio.java
@@ -4,50 +4,65 @@ import Gestore.Autocarro;
 import Gestore.Merce;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.ListIterator;
 
 
 public class Viaggio {
-    public Città Partenza;
-    public Città Destinazione;
+    ArrayList ship = new ArrayList();
+
+    public Percorso percorso;
     public Autocarro veicoloInUso;
     public Merce merceTrasportata;
 
-    public Viaggio(Città from, Città to) {
-        Partenza = from;
-        Destinazione = to;
+    public Viaggio(Percorso p) {
+        this.percorso = p;
     }
 
     public void addVehicle(Autocarro e) {
         veicoloInUso = e;
+        ship.add(veicoloInUso);
     }
 
     public void addMerce(Merce e) {
         merceTrasportata = e;
+        ship.add(merceTrasportata);
     }
 
-    /**
-     * Method that retrieve information about the Viaggio and it stores all the infos in an Arraylist
-     */
-    public ArrayList retrieveInfo(){
-        ArrayList info = new ArrayList();
-        info.add(Partenza.nome);
-        info.add(Destinazione.nome);
-        info.add(veicoloInUso.Targa);
-        info.add(merceTrasportata.Quantità + "/" + veicoloInUso.Capacità);
-        info.add(merceTrasportata.type);
-        return info;
+
+
+    public void info() {
+        Iterator it = ship.iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next().toString() + ", ");
+        }
     }
 
-    public Città retrieveName() {
-        return Partenza;
-    }
 
-    public String retrieveTarga() {
-        return veicoloInUso.Targa;
-    }
 
-    public int retrieveMerceQuantity() {return merceTrasportata.retrieveQuantity();}
-
-    public Merce.Tipo retrieveMerce() {return merceTrasportata.retrieveTipo();}
+//    /**
+//     * Method that retrieve information about the Viaggio and it stores all the infos in an Arraylist
+//     */
+//    public ArrayList retrieveInfo(){
+//        ArrayList info = new ArrayList();
+//        info.add(Partenza.nome);
+//        info.add(Destinazione.nome);
+//        info.add(veicoloInUso.Targa);
+//        info.add(merceTrasportata.Quantità + "/" + veicoloInUso.Capacità);
+//        info.add(merceTrasportata.type);
+//        return info;
+//    }
+//
+//    public Città retrieveName() {
+//        return Partenza;
+//    }
+//
+//    public String retrieveTarga() {
+//        return veicoloInUso.Targa;
+//    }
+//
+//    public int retrieveMerceQuantity() {return merceTrasportata.retrieveQuantity();}
+//
+//    public Merce.Tipo retrieveMerce() {return merceTrasportata.retrieveTipo();}
 
 }
