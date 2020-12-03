56280a8153c0e4156df161d1331d6fce45497cc2
Cristian Murtas <skylerhunter6@gmail.com>
2020-12-03 11:14:13 +0100

    Added MISSION for the class

--- src/Gestore/GestoreViaggi.java
+++ src/Gestore/GestoreViaggi.java
@@ -6,6 +6,13 @@ import java.util.List;
 
 public class GestoreViaggi {
 
+    /**
+     * MISSION: questa classe deve permettere la raccolta di tutte le istanze di Viaggio
+     * in modo tale da poter ricerca per parametro, le spedizioni correlate
+     * es. : voglio poter cercare tutte le spedizioni che partono da A
+     *       voglio poter cercare la spedizione effettuata dal camion con targa "abc"
+     */
+
 
 
 
