151d0d4fed4fa004348e3ceaaeb932291f8428e6
Cristian Murtas <skylerhunter6@gmail.com>
2020-12-03 11:13:58 +0100

    Added a method for printing the elements in this

--- src/Gestore/Percorso.java
+++ src/Gestore/Percorso.java
@@ -32,6 +32,12 @@ public class Percorso {
         this.percorso.add(percorso.size() - 1, c);
     }
 
+    /**
+     * Permette di cambiare una tappa all'interno di un'istanza di Percorso
+     * @param c la citta da inserire
+     * @param r la citta da sostituire
+     * @throws CityNotFoundException quando la citta da sostituire non e' presente in this
+     */
     public void changeTappa(Città c, Città r) throws CityNotFoundException {
         if (percorso.contains(r)) {
             for (int i = 0; i < percorso.size(); i++) {
@@ -45,86 +51,19 @@ public class Percorso {
     }
 
     /**
-     * Mostra la lista dei percorsi disponibili
+     * Permette di visionare gli oggetti presenti in this su standard output
      */
-    public void showPercorsi() {
+    public void printPercorso() {
         for (int i = 0; i < percorso.size(); i++) {
-            String nomeCitta = percorso.get(i).nome;
-            System.out.println(nomeCitta + ", ");
+            System.out.print(percorso.get(i).toString() + ", ");
         }
     }
 
-    public void search(Città c) {
-        Iterator it = percorso.iterator();
-        while (it.hasNext()) {
-            Object x = it.next();
-            if (x instanceof Città)
-                System.out.println(c.nome);
-        }
-    }
-
-    public String toString() {
-        StringBuffer sb = new StringBuffer();
-        Iterator<Città> it = percorso.iterator();
-        while (it.hasNext()) {
-            String city = it.next().nome;
-            sb.append(city + ", ");
-        }
-        return sb.toString();
-    }
 
 
-//
-//    /**
-//     * Method to see every single Viaggio
-//     */
-//    public void showList(){
-//        int length = this.percorsi.size();
-//        ArrayList info = new ArrayList();
-//        for (int i = 0; i < length; i++) {
-//            info = percorsi.get(i).retrieveInfo();
-//            System.out.println(Arrays.toString(info.toArray()));
-//        }
-//    }
-//
-//    /**
-//     * Method to search a Prenotazione, it returns the trip with the Città c associated
-//     * @param c the Città used to retrieve inforamtion about
-//     */
-//    public void cercaPrenotazione(Città c) {
-//        int length = this.percorsi.size();
-//        ArrayList result = new ArrayList();
-//        for (int i = 0; i < length; i++) {
-//            if (c == percorsi.get(i).retrieveName()) {
-//                result.add(percorsi.get(i).retrieveInfo());
-//            }
-//        }
-//        System.out.println(Arrays.toString(result.toArray()));
-//
-//    }
-//
-//    public void cercaPrenotazione(String t) {
-//        int length = this.percorsi.size();
-//        ArrayList result = new ArrayList();
-//        for (int i = 0; i < length; i++) {
-//            if (t == percorsi.get(i).retrieveTarga()) {
-//                result.add(percorsi.get(i).retrieveInfo());
-//            }
-//        }
-//        System.out.println(Arrays.toString(result.toArray()));
-//
-//    }
-//
-//    public void cercaPrenotazione(Merce.Tipo m) {
-//        int length = this.percorsi.size();
-//        ArrayList result = new ArrayList();
-//        for (int i = 0; i < length; i++) {
-//            if (m == percorsi.get(i).retrieveMerce()) {
-//                result.add(percorsi.get(i).retrieveInfo());
-//            }
-//        }
-//        System.out.println(Arrays.toString(result.toArray()));
-//    }
+
+
+
 
 
 }
