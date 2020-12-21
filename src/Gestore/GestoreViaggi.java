b9bfb707418f3f6c8c1b29a6610a2367ce0d479f
Cristian Murtas <skylerhunter6@gmail.com>
2020-12-21 11:12:43 +0100

    Given a first implementation for this class + first method with .stream for retrieving certain Viaggio object

--- src/Gestore/GestoreViaggi.java
+++ src/Gestore/GestoreViaggi.java
@@ -1,19 +1,43 @@
 package Gestore;
 
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
+import java.util.Vector;
+import java.util.stream.Collectors;
 
 public class GestoreViaggi {
 
     /**
      * MISSION: questa classe deve permettere la raccolta di tutte le istanze di Viaggio
-     * in modo tale da poter ricerca per parametro, le spedizioni correlate
+     * in modo tale da poter ricercare per parametro, le spedizioni correlate
      * es. : voglio poter cercare tutte le spedizioni che partono da A
      *       voglio poter cercare la spedizione effettuata dal camion con targa "abc"
      */
 
+    List<Viaggio> prenotazioni;
 
+    /** Constructor */
+    public GestoreViaggi() {
+        prenotazioni = new Vector<Viaggio>();
+    }
+
+    /**
+     * Aggiunge un' istanza di Viaggio a this
+     * @param v
+     */
+    public void addViaggio(Viaggio v) {
+        this.prenotazioni.add(v);
+    }
+
+    /**
+     *
+     * @param p citta per la quale si vuole cercare una o piu' istanze di Viaggio
+     * @return una lista con tutte le istanze di Viaggio con Viaggio.partenza == p
+     */
+    public List<Viaggio> cercaPrenotazione(Città p) {
+        return prenotazioni.stream()
+                .filter(viaggio -> viaggio.getPercorso().getPartenza() == p )
+                .collect(Collectors.toList());
+    }
 
 
 }
