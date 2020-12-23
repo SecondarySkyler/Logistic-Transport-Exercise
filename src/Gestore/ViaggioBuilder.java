04b95c78f6f7584af091c804e9c67af4c0e1bd06
Cristian Murtas <skylerhunter6@gmail.com>
2020-12-23 14:33:40 +0100

    Added class Builder for Viaggio

---
+++ src/Gestore/ViaggioBuilder.java
@@ -0,0 +1,46 @@
+package Gestore;
+
+public class ViaggioBuilder {
+
+    //Citta
+    private String partenza;
+    private String destinazione;
+    //Autocarro
+    private String targa;
+    private int capacity;
+    private Merce.Tipo tipoDiMerce;
+    //Merce
+    private int quantita;
+    private Merce.Tipo tipo;
+
+    //Oggetti necessari per la creazione di un'istanza di Viaggio
+    private Percorso percorso;
+    private Autocarro veicoloInUso;
+    private Merce merceTrasportata;
+
+
+    private ViaggioBuilder(String p, String d) {
+        this.percorso = new Percorso(new Città(p), new Città(d));
+
+    }
+
+    /** Constructor */
+    public static ViaggioBuilder newBuilder(String p, String d) {
+        return new ViaggioBuilder(p,d);
+    }
+
+    public ViaggioBuilder withVehicle (String t, int c, Merce.Tipo mt) {
+        this.veicoloInUso = new Autocarro(t, c, mt);
+        return this;
+    }
+
+    public ViaggioBuilder willShip (int q, Merce.Tipo m) {
+        this.merceTrasportata = new Merce(q, m);
+        return this;
+    }
+
+    public Viaggio build () {
+        return new Viaggio(percorso, veicoloInUso, merceTrasportata);
+    }
+
+}
